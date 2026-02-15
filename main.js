3af495575981ab6a17fbac2431d4797cf5acc9d2
nap661 <kelly.anne1@nhs.net>
2026-02-15 06:50:26 +0000

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-
-const navToggle = document.getElementById('navToggle');
-const menu = document.getElementById('menu');
-if (navToggle && menu){
-  navToggle.addEventListener('click', () => {
-    menu.classList.toggle('open');
-  });
-}
